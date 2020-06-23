8f44b2a50afd448e3fb581a60da78b436e75ebb7
ecoshub <emrecanocak@windowslive.com>
2020-06-23 21:49:50 +0300

    dataseeding imp. done sucss.

--- ProductService/Startup.cs
+++ ProductService/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -46,7 +47,14 @@ namespace ProductService {
                 endpoints.MapControllers ();
             });
 
-            DatabaseSeed.Seeding.Seed (context);
+            // * Database seeding code block.
+            try {
+                DatabaseSeed.Seeding.Seed (context);
+            } catch (DbUpdateException) {
+                Console.WriteLine ("Dataseed already exists.");
+            } catch (Exception) {
+                Console.WriteLine ("Dataseeding problem!");
+            }
         }
     }
 }

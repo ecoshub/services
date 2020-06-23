0d3dd7e5c77ed950972eaa3c886d2f9f97909813
ecoshub <emrecanocak@windowslive.com>
2020-06-23 22:12:25 +0300

    get product, get all products and custom error object imp.

--- ProductService/Controllers/ProductController.cs
+++ ProductService/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using ProductService.Contexts;
 using ProductService.DataTransferObjects;
 using ProductService.Models;
 using ProductService.Repositories;
+using ProductService.Tools;
 
 namespace ProductService.Controllers {
     [Route ("/api/products")]
@@ -16,14 +18,48 @@ namespace ProductService.Controllers {
         private readonly ProductDatabaseContext _context;
         private readonly IProductRepository _repo;
 
+        private string alreadyExistsHeader = "Already Exists";
+        private string alreadyExistsBody = "The Product you have provided is already exists.";
+        private string alreadyExistsCode = "PE001";
+
         public ProductController (IProductRepository repo, ProductDatabaseContext context, IMapper mapper) {
             _repo = repo;
             _context = context;
             _mapper = mapper;
         }
 
-        // add product endpoint
-        // "/api/products"
+        // * get product endpoint
+        // * "/api/products/{product_id}"
+        [HttpGet ("{product_id}")]
+        [ProducesResponseType (400)]
+        [ProducesResponseType (200)]
+        public ActionResult<product> getProduct (Guid product_id) {
+            product selected = _repo.getProduct (product_id);
+            if (selected == null) {
+                return NotFound ();
+            }
+            return Ok (selected);
+        }
+
+        // * get all product
+        // * "/api/products"
+        // * This endpoint is created for testing/debugging this service.
+        [HttpGet]
+        [ProducesResponseType (400)]
+        [ProducesResponseType (200)]
+        public ActionResult getAllProducts () {
+            if (!ModelState.IsValid) {
+                return BadRequest (ModelState);
+            }
+            IEnumerable<product> all = _repo.getAllProducts ();
+            if (all == null) {
+                return NotFound ();
+            }
+            return Ok (all);
+        }
+
+        // * add product endpoint
+        // * "/api/products"
         [HttpPost]
         [ProducesResponseType (typeof (product), (int) HttpStatusCode.OK)]
         [ProducesResponseType (400)]
@@ -33,7 +69,11 @@ namespace ProductService.Controllers {
                 return BadRequest (ModelState);
             }
             if (_repo.productExistsByName (newProduct.productName)) {
-                return NotFound (ModelState);
+                CustomError productAlreadyExists = new CustomError (
+                    alreadyExistsHeader,
+                    alreadyExistsBody,
+                    alreadyExistsCode);
+                return BadRequest (productAlreadyExists);
             }
             if (!ModelState.IsValid) {
                 return BadRequest (ModelState);

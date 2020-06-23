ddee80d7bc7113bf0f1278ce46b5eb7fe127344b
ecoshub <emrecanocak@windowslive.com>
2020-06-23 21:42:38 +0300

    product.productName is now unique

--- ProductService/Contexts/ProductDatabaseContext.cs
+++ ProductService/Contexts/ProductDatabaseContext.cs
@@ -8,6 +8,12 @@ namespace ProductService.Contexts {
             options.UseNpgsql ("Host=localhost;Database=product_db;Username=postgres");
         }
 
+        protected override void OnModelCreating (ModelBuilder builder) {
+            builder.Entity<product> ()
+                .HasIndex (u => u.productName)
+                .IsUnique ();
+        }
+
         public virtual DbSet<product> product { get; set; }
     }
 }

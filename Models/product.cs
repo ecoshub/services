bef57804f043c940be2976c08c1ee2c53d9a5910
ecoshub <emrecanocak@windowslive.com>
2020-06-23 18:20:17 +0300

    .git folder change for making global

--- Models/product.cs
+++ Models/product.cs
@@ -16,25 +16,26 @@ namespace ProductService.Models {
         [Required]
         public double productPrice { get; set; }
 
-        [DefaultValue ("initial product desciription")]
-        public string productDesciption { get; set; }
+        [DefaultValue ("initial product description")]
+        public string productDescription { get; set; }
 
         [Required]
         public DateTime productRegisterDate { get; set; }
+
+        public product () { }
+
+        public product (
+            string product_name,
+            uint product_stock,
+            double product_price,
+            string product_description) {
+            productId = Guid.NewGuid ();
+            productName = product_name;
+            productStock = product_stock;
+            productPrice = product_price;
+            productDescription = product_description;
+            productRegisterDate = DateTime.Now;
+        }
     }
 
-    public product () { }
-
-    public product (
-        string product_name,
-        uint product_stock,
-        double product_price,
-        string product_description) {
-        productId = Guid.NewGuid ();
-        productName = product_name;
-        productStock = product_stock;
-        productPrice = product_price;
-        productDesciption = product_description;
-        productRegisterDate = DateTime.Now;
-    }
 }
---
+++ Models/sale.cs
@@ -0,0 +1,41 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductService.Models {
+    public class sale {
+        [Required]
+        public Guid billId { get; set; }
+
+        [Key]
+        public Guid saleId { get; set; }
+
+        [ForeignKey ("product")]
+        public Guid productRefId { get; set; }
+
+        [Required]
+        public DateTime saleDate { get; set; }
+
+        [Required]
+        public uint saleAmount { get; set; }
+
+        [Required]
+        public double saleUnitPrice { get; set; }
+
+        [Required]
+        public double saleTotalPrice { get; set; }
+
+        public sale () { }
+
+        public sale (Guid _billId, Guid _productRefId, DateTime _saleDate, uint _saleAmount, double _saleUnitPrice, double _saleTotalPrice) {
+            billId = _billId;
+            saleId = Guid.NewGuid ();
+            productRefId = _productRefId;
+            saleDate = _saleDate;
+            saleAmount = _saleAmount;
+            saleUnitPrice = _saleUnitPrice;
+            saleTotalPrice = _saleAmount * _saleUnitPrice;
+        }
+    }
+}

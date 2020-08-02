5b850148a8856798648fc9416b31a7d3eda0b095
ecoshub <emrecanocak@windowslive.com>
2020-08-02 17:54:28 +0300

    format changes

--- BillingService/Repositories/BillinRepository.cs
+++ BillingService/Repositories/BillinRepository.cs
@@ -11,26 +11,26 @@ namespace BillingService.Repositories {
 
         private readonly BillingDatabaseContext _context;
 
-        public BillingRepository (BillingDatabaseContext context) {
+        public BillingRepository(BillingDatabaseContext context) {
             _context = context;
         }
 
-        public List<sale> getBill (Guid billId) {
-            return _context.sale.AsNoTracking ().Where (b => b.billId == billId).ToList ();
+        public List<sale> getBill(Guid billId) {
+            return _context.sale.AsNoTracking().Where(b => b.billId == billId).ToList();
         }
 
-        public void invoice (List<Guid> product_ids) {
-            throw new NotImplementedException ();
+        public void invoice(List<Guid> product_ids) {
+            throw new NotImplementedException();
         }
 
-        public void addInvoice (sale _sale) {
-            _context.Add (_sale);
-            _context.SaveChanges ();
-            _context.Entry (_sale).State = EntityState.Detached;
+        public void addInvoice(sale _sale) {
+            _context.Add(_sale);
+            _context.SaveChanges();
+            _context.Entry(_sale).State = EntityState.Detached;
         }
 
-        public List<Guid> getBills () {
-            List<Guid> bills = _context.sale.Select (b => b.billId).ToList ();
+        public List<Guid> getBills() {
+            List<Guid> bills = _context.sale.Select(b => b.billId).ToList();
             return bills;
         }
     }
